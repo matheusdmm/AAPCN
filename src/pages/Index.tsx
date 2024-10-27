282cedfdb887246f147674a2914d450be82beee4
Matheus <matheusdmm@users.noreply.github.com>
2024-10-27 00:20:38 -0300

    cleanup

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,5 @@
 import Header from '../components/Header';
 
-const headerImg =
-  'https://images.unsplash.com/photo-1506463108611-88834e9f6169?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-
 const headerImgs = [
   'https://images.unsplash.com/photo-1506463108611-88834e9f6169?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://placeholder.com/1920x1080',
